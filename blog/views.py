6cd718b8fc3465f0b0b3078589fc4cea3f5918b5
Yamila Moreno <yamila.ms@gmail.com>
2015-08-17 23:37:41 +0200

    Minor fixes

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(
         published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
@@ -18,18 +18,12 @@ def post_detail(request, pk):
 def post_new(request):
   if request.method == "POST":
       form = PostForm(request.POST)
-      print("*************************** POST")
-      print(form)
-      print(form.errors)
       if form.is_valid():
-          print("*************************** VALID")
           post = form.save(commit=False)
           post.author = request.user
           post.published_date = timezone.now()
           post.save()
-          print(post)
           return redirect('blog.views.post_detail', pk=post.pk)
   else:
-      print("*************************** GET")
       form = PostForm()
   return render(request, 'blog/post_edit.html', {'form': form})
